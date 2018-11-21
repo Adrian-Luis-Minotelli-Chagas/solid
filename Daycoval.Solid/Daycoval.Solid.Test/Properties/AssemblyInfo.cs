b5da22b85b784136002da31fb2e9f29bbe236e75
adrian luis minotelli chagas <adrian.minotelli.ch@gmail.com>
2018-11-21 01:48:27 -0200

    Adicionado Projeto de Teste de Unidade

---
+++ Daycoval.Solid/Daycoval.Solid.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Daycoval.Solid.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Daycoval.Solid.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cd90ad8b-b246-4d29-b451-3b44cd705803")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
